2a68c7000b28468151af3f243a2806370970e75e
Vinicius Montuani <viniciusmontuani@gmail.com>
2025-05-31 17:05:13 -0300

    aparentemente feito(dessa vez acho que é sério)

---
+++ Contato/scripts.js
@@ -0,0 +1,15 @@
+var form = document.getElementById('contactForm');
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    Swal.fire({
+        icon: 'success',
+        title: 'Mensagem enviada!',
+        text: 'Obrigado por entrar em contato. Retornaremos em breve.',
+        showConfirmButton: false,
+        timer: 3000
+    });
+    setTimeout(function() {
+        window.location.href = '../index.html';
+    }, 3000);
+});
---
+++ Termos de uso/scripts.js
@@ -0,0 +1,16 @@
+document.getElementById('acceptForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    if (document.getElementById('acceptTerms').checked) {
+        Swal.fire({
+          icon: 'success',
+          title: 'Termos aceitos!',
+          text: 'Obrigado por aceitar os Termos de Uso e a Política de Tratamento de Dados.',
+          confirmButtonColor: '#f7b600',
+          confirmButtonText: 'Continuar'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = '../index.html';
+          }
+        });
+      }
+    });
